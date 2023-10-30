7c4a704aefa3af66bf31027edfb08601efa19e07
hacer-yalcin <haceryalcnn@gmail.com>
2023-10-30 22:39:07 +0300

    homework -bonus

---
+++ homework-bonus/döngü.py
@@ -0,0 +1,12 @@
+krediler =["Hızlı Kredi", "Maaşını Halkbank'tan alanlara özel", "Mutlu Emekli İhtiyaç Kredisi"]
+
+for kredi in krediler:
+    print(krediler)
+
+for i in range(len(krediler)):
+    print(krediler[i])
+for i in range(3,10):
+    print(i)
+for i in range(0,11,2):
+    print(i)
+   
